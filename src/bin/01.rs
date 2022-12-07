55c0d5bfa2f5fa020419163a8c350d2c7c6d7d71
Michael Fox <mjfox314@gmail.com>
2022-12-07 08:18:38 -0500

    solution: day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,16 +1,30 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use itertools::Itertools;
+use std::cmp::Reverse;
+
+type Input = Vec<u32>;
+
+fn parse(input: &str) -> Input {
+    input
+        .split("\n\n")
+        .map(|c| c.lines().filter_map(|l| l.parse::<u32>().ok()).sum())
+        .collect()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(calorie_counts: Input) -> Option<u32> {
+    calorie_counts.iter().max().copied()
 }
 
-fn main() {
-    let input = &advent_of_code::read_file("inputs", 1);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
+pub fn part_two(calorie_counts: Input) -> Option<u32> {
+    Some(
+        calorie_counts
+            .iter()
+            .sorted_by_key(|x| Reverse(*x))
+            .take(3)
+            .sum(),
+    )
 }
+
+advent_of_code::main!(1);
 
 #[cfg(test)]
 mod tests {
@@ -18,13 +32,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        let result = part_one(parse(&advent_of_code::read_file("examples", 1)));
+        assert_eq!(result, Some(24000));
     }
 
     #[test]
     fn test_part_two() {
-        let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        let result = part_two(parse(&advent_of_code::read_file("examples", 1)));
+        assert_eq!(result, Some(45000));
     }
 }
